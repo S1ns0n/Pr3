69e3bddaeb18313fd99836ef4d5eb4d73d0d8726
Валентина <vgagarkina06@mail.ru>
2025-05-26 14:43:32 +0900

    initial commit

---
+++ Pr3_IGORA/Pr3_IGORA/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Pr3_IGORA.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pr3_IGORA
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //FrameApp.frmObj = MainFrame;
+            //FrameApp.frmObj.Navigate(new);
+
+
+        }
+
+        private void test_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("AAAAAAAAAAAAAAAAA");
+
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            
+        }
+    }
+}
